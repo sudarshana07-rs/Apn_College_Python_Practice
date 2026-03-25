d86f378a886c7e4a131dd564c6e506e147a5109b
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-25 19:35:28 +0530

    Prac_1

---
+++ Practic_1.py
@@ -0,0 +1,12 @@
+# Practice Session 1 
+print("Hello World!!1")
+print('Sudarshana.R.S')
+print('''My age is 18''')
+college='Sapthagiri NPS University'
+place= '''Bangalore'''
+print('College: ', college , '\tPlace: ', place)
+phno= 6065897646576
+print(f'Phone number: {phno}')
+age=18
+age2=18
+print('My age is: ', age2)
