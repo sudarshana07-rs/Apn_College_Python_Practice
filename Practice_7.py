0a1be2d41ce3fece555b9c1ee726cb5fb5f216c0
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-02 19:10:12 +0530

    commit

---
+++ Practice_7.py
@@ -0,0 +1,4 @@
+# Calculate simple intrest 
+P=float(input("Enter the Principle Amount: "))
+T=float(input("Enter the Time period (in years): "))
+R=float(input("Enter the Rate of Intrest (in %): "))
