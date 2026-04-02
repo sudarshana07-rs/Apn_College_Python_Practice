3e5e80bf0130644dbf7bfcd9ce7d37804f029106
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-02 19:12:49 +0530

    commit

--- Practice_7.py
+++ Practice_7.py
@@ -3,3 +3,4 @@ P=float(input("Enter the Principle Amount: "))
 T=float(input("Enter the Time period (in years): "))
 R=float(input("Enter the Rate of Intrest (in %): "))
 S=(P*T*R)/100
+print(f'Simple Intrest= {S}')
